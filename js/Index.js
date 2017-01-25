23e24345099cdc55062ac51d4f34ad3e81ee8beb
Mrtokko96 <Mrtokko96@gmail.com>
2017-01-26 01:46:51 +0400

    Add files via upload

---
+++ js/Index.js
@@ -0,0 +1,8 @@
+//open butt
+function myFunction() {
+  document.getElementById("modalbox").style.transform = "translateY(105%)";
+}
+//close butt
+function FunctionClose() {
+  document.getElementById("modalbox").style.transform = "translateY(-90%)";
+}
